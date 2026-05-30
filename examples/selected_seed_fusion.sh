0ac5bb7c6e11864d82f71e7455ef8b9e65f55730
jts0926 <59254800+jts0926@users.noreply.github.com>
2026-05-30 12:10:43 +0800

    Update selected_seed_fusion.sh

--- examples/selected_seed_fusion.sh
+++ examples/selected_seed_fusion.sh
@@ -7,4 +7,4 @@ python risk_score_fusion.py \
   --run-id "${RUN_ID}" \
   --seed 11 \
   --split-model patch_mil_m30_pa \
-  --model-seeds m30_pa=23,m30_lat=11,bl_pa=73,bl_lat=51
+  --model-seeds m30_pa=11,m30_lat=11,bl_pa=11,bl_lat=11
